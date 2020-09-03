6d491f596f367cf97ca35af65f7ea67a6d1481ef
rizki maulana <rizkimaulana4106@gmail.com>
2020-09-03 08:49:45 +0700

    rename package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'bingsearchpy',
   packages = ['bingsearchpy'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'bing search engine for python',
   author = 'mrxxx04',
