34ab3856c40bcb90e34d789bb5828c01cac02900
rizki maulana <rizkimaulana4106@gmail.com>
2020-09-03 20:37:23 +0700

    fix broken when return data

--- bingsearchpy/bingsearch.py
+++ bingsearchpy/bingsearch.py
@@ -8,55 +8,65 @@ class BingSearch:
         self.data = []
         self.page = 0
 
+    def get_total_page(self, url):
+
+        no = random.randint(0,len(useragent) - 1)
+        req = requests.get(url, headers={"User-Agent": useragent[no]}).content
+        soup = BeautifulSoup(req, 'html.parser')
+        result_count = soup.find('span', class_ = 'sb_count').get_text().split(' ')[0].split(',')
+        pages_count = ''
+        for page in result_count:
+            pages_count += str(page)
+        self.page = int(pages_count)
+
     
     def request_data(self, url):
-
+        """
+        used for request data and DO NOT USE this method directly from your code
+        """
+        no = random.randint(0,len(useragent) - 1)
+        user_agent = useragent[no]
         try:
-            # request data
-            no = random.randint(0,len(useragent) - 1)
-            page = 1
-            req = requests.get(url, headers={"User-Agent": useragent[no]}).content
+            # get total search result
+            self.get_total_page(url)
 
-            # start scraping data
-            soup = BeautifulSoup(req, 'html.parser')
-            result_count = soup.find('span', class_ = 'sb_count').get_text().split(' ')[0].split(',')
-            if len(result_count) > 0:
-                container_count = ""
-                for count in result_count:
-                    container_count += str(count)
-                # final_count = int(container_count)
-                # self.page = final_count
+            # handling large data
+            repeat = 0
+            if self.page >= 20:
+                repeat = 20
+            else:
+                repeat = self.page
 
-            # parsing data ( title, link, description )
-            final_data = []
-            index = 0
-            for data in soup.find_all(class_ = 'b_algo'):
-                index += 1
-                main = data.find('h2').find('a')
-                list_data = {}
-                list_data['no'] = index
-                list_data['title']  = main.get_text()
-                list_data['link']   = main.attrs['href']
-                final_data.append(list_data)
-            return final_data
+            # generating search result
+            for i in range(1,repeat):
+                # request data
+                
+                req = requests.get(url + "&first=" + str(i), headers={"User-Agent": user_agent}).content
+
+                # start scraping data
+                soup = BeautifulSoup(req, 'html.parser')
+                result_count = soup.find('span', class_ = 'sb_count').get_text().split(' ')[0].split(',')
+
+                # parsing data ( title, link, description )
+
+                for data in soup.find_all(class_ = 'b_algo'):
+                    main = data.find('h2').find('a')
+                    list_data = {}
+                    list_data['no'] = i
+                    list_data['title']  = main.get_text()
+                    list_data['link']   = main.attrs['href']
+                    yield list_data
+
         except:
             pass
-        
 
     def search(self, query):
         """
-        search data with random user agent
+        search data with random user agent.
+        query : what kind topic that you want to find
         """
-        url = "https://www.bing.com/search?q=" + query + "&first="
-        index = 1
-        res = []
-        for i in range(1,5):
-            try:
-                for data in self.request_data(url + str(i)):
-                    res.append(data)
-            except:
-                pass
-        return res
+        url = "https://www.bing.com/search?q=" + query
+        return self.request_data(url)
 
 if __name__ != "__main__":
     engine = BingSearch()
